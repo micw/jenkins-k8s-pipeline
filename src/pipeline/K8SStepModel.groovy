b9f0466c1170a385b0d3be0a70e2f40054cec4c6
Michael Wyraz <mw@brick4u.de>
2021-03-30 10:36:23 +0200

    Fix for k8s step

--- src/pipeline/K8SStepModel.groovy
+++ src/pipeline/K8SStepModel.groovy
@@ -10,13 +10,13 @@ class K8SStepModel extends AbstractStepModel {
 
 	@Override
 	List getExtraContainers(config) {
-		return [
+		return [[
 			name:"k8s",
 			image: 'dtzar/helm-kubectl:3.1.2',
 			command: 'cat',
 			ttyEnabled: true,
 			alwaysPullImage: true
-		]
+		]]
 	}
 
 	void doExecute(config,Map globals) {
